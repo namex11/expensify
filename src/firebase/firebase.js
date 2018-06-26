ba65d3b9a995b4b4821fe6a7dd799cec009a83ee
Mindaugas <33827682+namex11@users.noreply.github.com>
2018-06-26 14:13:15 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDdPkp7MVKZ5fTgyPX5_WhR-825HjqV28M",
+    apiKey: "",
     authDomain: "expensify-65af3.firebaseapp.com",
     databaseURL: "https://expensify-65af3.firebaseio.com",
     projectId: "expensify-65af3",
