c9fa4b9b7bd38a14cfba663ecd9017791112a0b9
Mindaugas <mkestenis@gmail.com>
2018-06-28 20:27:37 +0300

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,34 +8,40 @@ const config = {
     storageBucket: "expensify-65af3.appspot.com",
     messagingSenderId: ""
 };
+
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Mindaugas',
-    age: 32,
-    stressLevel: 6,
-    job: {
-        title: 'software developer',
-        company: 'google'
-    },
-    location: {
-        city: 'Vilnius',
-        country: 'Lietuva'
-    }
-}).then(() => {
-    console.log('Data is saved')
-}).catch((error) => {
-    console.log('This failed.', error)
-});
+database.ref().on('value', (dataSnapshot)=>{
+    const data = dataSnapshot.val();
+    console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+})
 
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'amazon',
-    'location/city': 'Seattle'
-}).then(()=>{
-    console.log('Data updated')
-}).catch((error)=>{
-    console.log('Something went wrong', error)
-});
+// database.ref().set({
+//     name: 'Mindaugas',
+//     age: 32,
+//     stressLevel: 6,
+//     job: {
+//         title: 'software developer',
+//         company: 'google'
+//     },
+//     location: {
+//         city: 'Vilnius',
+//         country: 'Lietuva'
+//     }
+// }).then(() => {
+//     console.log('Data is saved')
+// }).catch((error) => {
+//     console.log('This failed.', error)
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'amazon',
+//     'location/city': 'Seattle'
+// }).then(() => {
+//     console.log('Data updated')
+// }).catch((error) => {
+//     console.log('Something went wrong', error)
+// });
