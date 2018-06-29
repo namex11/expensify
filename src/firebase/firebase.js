4a761732a4e5ca9270242c54b3f0de5445d33240
Mindaugas <mkestenis@gmail.com>
2018-06-29 16:31:07 +0300

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,16 +7,51 @@ const config = {
     projectId: "expensify-65af3",
     storageBucket: "expensify-65af3.appspot.com",
     messagingSenderId: ""
-};
+  };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().on('value', (dataSnapshot)=>{
-    const data = dataSnapshot.val();
-    console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+// database.ref('expenses').on('value', (dataSnapshot)=>{
+//     const expenses = [];
+//     dataSnapshot.forEach((childSnapshot)=>{
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+//     console.log(expenses);
+// });
+
+database.ref('expenses').on('child_changed', (dataSnapshot)=>{
+    console.log(dataSnapshot.key, dataSnapshot.val())
 })
+
+
+
+// database.ref('expenses').once('value').then((dataSnapshot)=>{
+//     const expenses = [];
+//     dataSnapshot.forEach((childSnapshot)=>{
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+//     console.log(expenses)
+// });
+
+// database.ref('expenses').push({
+//     description: 'beer',
+//     note: 'Some note',
+//     amount: 46,
+//     createdAt: 0
+// })
+
+// database.ref().on('value', (dataSnapshot)=>{
+//     const data = dataSnapshot.val();
+//     console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+// })
 
 // database.ref().set({
 //     name: 'Mindaugas',
