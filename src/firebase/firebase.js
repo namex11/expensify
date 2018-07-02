7af2a3e7d69198188df726b068abff39a7963b6e
Mindaugas <mkestenis@gmail.com>
2018-07-02 18:23:26 +0300

    deployed database for tests

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "",
-    authDomain: "expensify-65af3.firebaseapp.com",
-    databaseURL: "https://expensify-65af3.firebaseio.com",
-    projectId: "expensify-65af3",
-    storageBucket: "expensify-65af3.appspot.com",
-    messagingSenderId: ""
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
@@ -59,7 +59,7 @@ export { firebase, database as default };
 //     age: 32,
 //     stressLevel: 6,
 //     job: {
-//         title: 'software developer',
+//         title: 'developer',
 //         company: 'google'
 //     },
 //     location: {
